22a955de1616bbd148e64c49f2eaeea7230fa3b2
Gilson Gabriel <gilson@codemastersolucoes.com>
2020-12-28 17:05:12 +0000

    alteração na rota padrão

--- config/code-acl-web.php
+++ config/code-acl-web.php
@@ -2,6 +2,6 @@
 
 return [
     'domain' => env('CODEACL_DOMAIN', null),
-    'path' => env('CODEACL_PATH', '/cms/code-acl-web'),
+    'path' => env('CODEACL_PATH', 'cms/code-acl-web'),
     'middleware' => 'web'
 ];
